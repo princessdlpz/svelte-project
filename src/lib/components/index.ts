b9bec34290e3e55bcab2cede504bfbae755f66d9
Princess Rosette Dela Paz <princessrosettedelapaz@gmail.com>
2024-05-10 19:14:29 +0200

    Minor ui additions
    Icons, small transitions, color highlights

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,5 @@
 export { default as LogoutButton } from './LogoutButton.svelte';
 export { default as Button } from './Button.svelte';
+export { default as Navigation } from './Navigation.svelte';
+export { default as Header } from './Header.svelte';
+export { default as IconButton } from './IconButton.svelte';
